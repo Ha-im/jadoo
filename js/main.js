e27a66db1205b2a6102521f953d871aab220d005
Ha-im <gungsun1@naver.com>
2025-05-02 12:30:50 +0900

    무한슬라이드

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ const testimonial_upBtn = document.querySelector(".testimonials .up");
 const testimonial_downBtn = document.querySelector(".testimonials .down");
 let testimonialIdx = 0;
 
-
 console.log(ongoing_percent);
 window.addEventListener("scroll",()=>{
     if(window.scrollY - 300 > book_a_trip_OST - book_a_trip_height){
@@ -76,4 +75,41 @@ function changeTestimonial(idx){
     testimonialIdx = idx;
 }
 
+// 파트너 슬라이드
+//const partnersTotalWidth = document.querySelector("body").offsetWidth;
+const partnersTotalWidth = document.body.offsetWidth;
+const partners = document.querySelector(".partners ul");
+const partnerSlides=partners.querySelectorAll('li');
+const partnerSlidesWidth = partnersTotalWidth/partnerSlides.length;
+//파티너 각 슬라이드 너비 지정
+for(let Slide of partnerSlides){
+    Slide.style.width = partnersTotalWidth +'px';
+}
+//복사본 생성
+/*
+a.innerHTML = B // a요소에 b를 태그로 생성, 기존 내용을 제거 b내용 교체
+let a = b.innerHTML // b안에 내용을 HTML 태그로 변수명 a에 할당
 
+//문자열 특정 문자 교체
+let x = a.replace(b,c) // a에서 b를 c로 교체 x에 할당
+//   /<li/g 정규표현식 이라고함
+*/
+let slideHTML = partners.innerHTML;
+console.log(slideHTML);
+let clonedSlide = slideHTML.replace(/<li/g,'<li class="clone"');
+console.log(clonedSlide);
+partners.innerHTML = clonedSlide + slideHTML;
+
+//리스트 전체가 배치되도록 부모너비 변경
+partners.style.width = partnersTotalWidth * 2 + 'px';
+//파트너 슬라이드 이동
+let partnerLeft = 0;
+function movePartners(){
+    // partnerLeft = partnerLeft -2;
+    partnerLeft -= 2
+    partners.style.left = partnerLeft + 'px';
+    //requestAnimationFrame(함수) 함수를 반복 실행 //
+    //requestAnimationFrame(movePartners); // 함수 반복실행
+    requestAnimationFrame(movePartners); // movepartners 함수내에서 재실행
+}
+requestAnimationFrame(movePartners); // movePartners 함수의 내용을 애니메이션으로 변경 (부드럽게 이동시켜주는역활)
