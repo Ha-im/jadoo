2e6faf40647f91948e721da53658b00e1a1bcff3
Ha-im <gungsun1@naver.com>
2025-05-02 11:07:38 +0900

    Implement testimonial pagination and navigation functionality

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ const book_a_trip_height = book_a_trip.offsetHeight;
 
 const ongoing_percent = document.querySelector(".ongoing .percent");
 const ongoing_bar = document.querySelector(".ongoing .bar");
+
+
+const testimonialList = document.querySelectorAll(".testimonials ul li");
+const testimonial_pager = document.querySelectorAll(".testimonials .pager a");
+const testimonial_upBtn = document.querySelector(".testimonials .up");
+const testimonial_downBtn = document.querySelector(".testimonials .down");
+let testimonialIndex = 0;
+
+
 console.log(ongoing_percent);
 window.addEventListener("scroll",()=>{
     if(window.scrollY - 300 > book_a_trip_OST - book_a_trip_height){
@@ -26,3 +35,30 @@ function satartNumberAnimation(){
         }
     }, 100);
 }
+
+    testimonial_pager.forEach((pager, index) => {
+        pager.addEventListener("click", (e) => {
+            e.preventDefault();
+            //모든페이저에서 acrive 클래스 제거
+            testimonial_pager.forEach((item) => {
+                item.classList.remove("active");
+            });
+            //클릭한 페이저에 active 클래스 추가
+            pager.classList.add("active");
+
+            testimonialList.forEach((item) => {
+                item.classList.remove("active");
+            });
+            testimonialList[index].classList.add("active");
+            testimonialIndex = index;
+        });
+    });
+testimonial_downBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (testimonialIndex < testimonialList.length - 1) {
+        testimonialIndex++;
+    } else {
+        testimonialIndex = 0;
+    }
+    changeTestimonial(testimonialIndex);
+})
